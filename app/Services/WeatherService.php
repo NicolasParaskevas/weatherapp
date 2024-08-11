ac52918c75586e289cc1e0a0a8b367a4d1e9fa77
Nicolas Paraskevas <nicolasparaskevas@hotmail.com>
2024-08-11 23:21:54 +0300

    write query for fetching avg temp from db

--- app/Services/WeatherService.php
+++ app/Services/WeatherService.php
@@ -6,9 +6,17 @@ use Illuminate\Support\Facades\DB;
 
 class WeatherService
 {
-    public function get(float $long, float $lat, string $time)
+    public function getTemperature(float $long, float $lat, string $time)
     {
         // get from DB
-        
+        $record = DB::table("weather_records")
+        ->join("locations", "locations.id", "=", "weather_records.location_id")
+        ->where("locations.longitude", "=", $long)
+        ->where("locations.latitude", "=", $lat)
+        ->where("weather_records.created_at", "=", $time)
+        ->pluck("weather_records.temperature")
+        ->first();
+
+        return $record;
     }
 }
