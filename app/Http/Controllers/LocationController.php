4d96b187f5de147d9c77935ee9785a1f518cf948
Nicolas Paraskevas <nicolasparaskevas@hotmail.com>
2024-08-11 23:21:30 +0300

    controller functions

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Services\WeatherService;
+use App\Location;
 
 class LocationController extends Controller
 {
@@ -20,6 +21,19 @@ class LocationController extends Controller
             "lat"  => "required",
             "time" => "required",
         ]);
+
+        $temperature = $this->weatherService->getTemperature(
+            $data["long"], $data["lat"], $data["time"]
+        );
+
+        if ($temperature === null)
+        {
+            // log error and respond
+        }
+
+        return response()->json([
+            "temperature" => $temperature
+        ]);
     }
 
     public function add(Request $request)
@@ -28,5 +42,15 @@ class LocationController extends Controller
             "long" => "required",
             "lat"  => "required",
         ]);
+
+        // Begin transaction
+
+        $location = new Location();
+        $location->longitude = $data['long'];
+        $location->latitude = $data['lat'];
+        $location->save();
+
+        // commit
+
     }
 }
