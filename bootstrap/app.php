5b5cfee51c4ee0d5223483ce84a5b30c7b22860a
Nicolas Paraskevas <nicolasparaskevas@hotmail.com>
2024-08-12 21:39:03 +0300

    timezone

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,7 +9,7 @@ use Monolog\Logger;
     dirname(__DIR__)
 ))->bootstrap();
 
-date_default_timezone_set(env('APP_TIMEZONE', 'EEST'));
+date_default_timezone_set(env('APP_TIMEZONE', 'Europe/Athens'));
 
 /*
 |--------------------------------------------------------------------------
